38ad9f19edada0110343b9c479f9505699481a46
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-15 20:45:26 +0530

    1 st  commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Sample data to pass to the template
+    image_url = request.args.get('image_url', '')
+    text = request.args.get('text', '')
+    rect_color = request.args.get('rect_color', '#FFFFFF')  # Red color
+
+    return render_template('index.html', image_url=image_url, text=text, rect_color=rect_color)
+
+if __name__ == '__main__':
+    app.run(debug=True)
