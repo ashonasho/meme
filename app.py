3753d0e92c545a149674a9f9c34edc7aaf7fe293
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-16 17:02:55 +0530

    chsange in full code

--- app.py
+++ app.py
@@ -7,10 +7,9 @@ def index():
     # Sample data to pass to the template
     image_url = request.args.get('image_url', '')
     text = request.args.get('text', '')
-    rect_color = request.args.get('rect_color', '#000000')  # Red color
-    
+    rect_color = request.args.get('rect_color', '#FFFFFF')  # Red color
 
-    return render_template('index.html', image_url=image_url, text=text,rect_color=rect_color)
+    return render_template('index.html', image_url=image_url, text=text, rect_color=rect_color)
 
 if __name__ == '__main__':
     app.run(debug=True)
