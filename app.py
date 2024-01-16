58b925ccff92359b29c3e099d325b0b37eeae217
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-16 07:25:39 +0530

    added text color

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ def index():
     image_url = request.args.get('image_url', '')
     text = request.args.get('text', '')
     rect_color = request.args.get('rect_color', '#FFFFFF')  # Red color
+    text_bottom = request.args.get('text', '')
+    text_color= request.args.get('rect_color', '#FFFFFF')
 
-    return render_template('index.html', image_url=image_url, text=text, rect_color=rect_color)
+    return render_template('index.html', image_url=image_url, text=text,rect_color=rect_color,text_bottom=text_bottom, text_color= text_color)
 
 if __name__ == '__main__':
     app.run(debug=True)
