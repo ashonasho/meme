f10dd2b7e905c6de2ec6ee5c59ea81eefb3c6497
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-17 11:20:02 +0530

    change in download button

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/display')
 def index():
-    # Sample data to pass to the template
+    # Retrieve parameters from the request
     image_url = request.args.get('image_url', '')
-    text = request.args.get('text', '')
-    rect_color = request.args.get('rect_color', '#000000')  # Red color
+    top_text = request.args.get('top_text', 'Top Text')  # Default value is 'Top Text'
+    bottom_text = request.args.get('bottom_text', 'Bottom Text')  # Default value is 'Bottom Text'
+    rect_color = request.args.get('rect_color', '#000000')  # Default rect color
 
-    return render_template('index.html', image_url=image_url, text=text, rect_color=rect_color)
+    # Pass the parameters to the template
+    return render_template('index.html', image_url=image_url, top_text=top_text, bottom_text=bottom_text, rect_color=rect_color)
 
 if __name__ == '__main__':
     app.run(debug=True)
