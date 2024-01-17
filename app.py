f16f5588d806b4f31600f40b3f8c11942070f22d
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-17 21:13:46 +0530

    added text color

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def index():
     top_text = request.args.get('top_text', '')  # Default value is 'Top Text'
     bottom_text = request.args.get('bottom_text', '')  # Default value is 'Bottom Text'
     rect_color = request.args.get('rect_color', '#000000')  # Default rect color
-
+    text_color= request.args.get('rect_color', '#FFFFFF')
     # Pass the parameters to the template
-    return render_template('index.html', image_url=image_url, top_text=top_text, bottom_text=bottom_text, rect_color=rect_color)
+    return render_template('index.html', image_url=image_url, top_text=top_text, bottom_text=bottom_text, rect_color=rect_color,text_color=text_color)
 
 
 
