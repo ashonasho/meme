39dddcc49662f6568145d5460b0c8963b5cf050a
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-17 21:11:39 +0530

    change text

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 def index():
     # Retrieve parameters from the request
     image_url = request.args.get('image_url', '')
-    top_text = request.args.get('top_text', 'Top Text')  # Default value is 'Top Text'
-    bottom_text = request.args.get('bottom_text', 'Bottom Text')  # Default value is 'Bottom Text'
+    top_text = request.args.get('top_text', '')  # Default value is 'Top Text'
+    bottom_text = request.args.get('bottom_text', '')  # Default value is 'Bottom Text'
     rect_color = request.args.get('rect_color', '#000000')  # Default rect color
 
     # Pass the parameters to the template
