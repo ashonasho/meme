14e6a42756bcd2c7b659cbd6dee569200c775563
ashonasho <ashonmcmillan1234@gmail.com>
2024-01-17 21:02:55 +0530

    change

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     # Retrieve parameters from the request
     image_url = request.args.get('image_url', '')
-    top_text = request.args.get('top_text', '')  # Default value is 'Top Text'
-    bottom_text = request.args.get('bottom_text', '')  # Default value is 'Bottom Text'
-    rect_color = request.args.get('rect_color', '#FFFFFF')  # Default rect color
+    top_text = request.args.get('top_text', 'Top Text')  # Default value is 'Top Text'
+    bottom_text = request.args.get('bottom_text', 'Bottom Text')  # Default value is 'Bottom Text'
+    rect_color = request.args.get('rect_color', '#000000')  # Default rect color
 
     # Pass the parameters to the template
     return render_template('index.html', image_url=image_url, top_text=top_text, bottom_text=bottom_text, rect_color=rect_color)
